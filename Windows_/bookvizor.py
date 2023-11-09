080bec58317e2127c660ea633d410692000610f2
Mr. Rodikolion <rodion2007a@mail.ru>
2023-11-09 18:53:20 +0200

    ver 2.1

--- Windows_/bookvizor.py
+++ Windows_/bookvizor.py
@@ -1,27 +1,133 @@
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot, Qt
-from PyQt5.QtGui import QImage, QPixmap, QPainter, QColor
+from PyQt5.QtGui import QImage, QPixmap, QPainter, QColor, QMouseEvent
 
 import cv2
 import pytesseract
+import os
 
 
 class BookVizor(QMainWindow):
     def __init__(self):
+        pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\Mr.Rodikolion\\PycharmProjects\\yandex_PyQt5\\tesseract/tesseract.exe"
         super().__init__()
         self.setupUi(self)
         self.startThreads()
         self.book_img_data = None
 
-        self.selectBtn.clicked.connect(self.update)
+        self.selectBtn.clicked.connect(self.selectImg)
+        self.deleteBtn.clicked.connect(self.deletePage)
+        self.saveTextBtn.clicked.connect(self.saveBook)
+
+        self.pages = []
+
+    def saveBook(self):
+        dir_name = QFileDialog.getExistingDirectory(self, 'Выбрать фото', '.')
+        img_dir = f'{dir_name}/pages_img'
+        try:
+            os.mkdir(img_dir)
+        except Exception:
+            pass
+
+        for y, filename in enumerate(os.listdir('../imgs/')):
+            file_path = os.path.join('../imgs/', filename)
+            os.replace(file_path, os.path.join(img_dir, filename))
+
+        text = self.textEdit.toPlainText()
+
+        fname = os.path.join(dir_name, 'text.txt')
+        with open(fname, mode='w') as f:
+            f.write(text)
+        self.statusbar.showMessage(f'Сохраненн файл: \"{fname}\"')
+
+    def loadText(self):
+        self.textEdit.setText('\n'.join(self.pages))
+
+    def loadTable(self):
+        self.tableWidget.setRowCount(0)
+        for y, filename in enumerate(os.listdir('../imgs/')):
+            file_path = os.path.join('../imgs/', filename)
+
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+
+            pixmap = QPixmap(file_path)
+            label = QtWidgets.QLabel()
+            label.setPixmap(pixmap.scaled(self.tableWidget.size().width(), self.tableWidget.size().width(),
+                                          Qt.KeepAspectRatio))
+            label.setAlignment(Qt.AlignCenter)
+
+            self.tableWidget.setCellWidget(y, 0, label)
+        self.tableWidget.resizeColumnsToContents()
+        self.tableWidget.resizeRowsToContents()
+
+    def deletePage(self):
+        try:
+            row = self.tableWidget.selectedIndexes()
+            if row != []:
+                row = row[0].row()
+            else:
+                return
+
+            self.tableWidget.removeRow(row)
+            del self.pages[row]
+            os.unlink(f'../imgs/page_{row}.jpg')
+
+            path_dir = sorted(os.listdir('../imgs/'), key=lambda x: int(x[x.index('_') + 1:-4]))
+            for n, filename in enumerate(path_dir):
+                file_path = os.path.join('../imgs/', filename)
+                new_file_path = file_path.replace(filename, f'page_{n}.jpg')
+                os.rename(file_path, new_file_path)
+
+            for n, page in enumerate(self.pages):
+                self.pages[n] = page.replace(page[page.index('\npage_'):], f'\npage_{n}')
+
+            self.loadText()
+            self.loadTable()
+        except Exception as e:
+            print(e)
+
+    def closeEvent(self, a0):
+        self.cam_th.stop()
+        self.hide()
+        for filename in os.listdir('../imgs/'):
+            file_path = os.path.join('../imgs/', filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
+
+        a0.accept()
+
+    def selectImg(self):
+        self.cam_th.isPaused = True
+
+        try:
+            if self.book_img_data[1] is not None:
+                cv2.imwrite(f'C:\\Users\\Mr.Rodikolion\\PycharmProjects\\yandex_PyQt5/imgs/page_{len(self.pages)}.jpg',
+                            self.book_img_data[1])
+
+                gray = cv2.cvtColor(self.book_img_data[1], cv2.COLOR_BGR2GRAY)
+                gray = cv2.medianBlur(gray, 3)
+                text = pytesseract.image_to_string(gray, lang='eng')
+
+                self.pages.append(f'{text}\npage_{len(self.pages)}')
+
+                self.loadText()
+                self.loadTable()
+
+
+        except Exception as e:
+            print(e)
+        self.cam_th.isPaused = False
 
     @pyqtSlot(tuple)
     def imgSliceWork(self, book_data):
         try:
-            self.book_img_data = book_data[1:]
+            self.book_img_data = book_data
 
             if book_data[1] is not None:
                 cv2.rectangle(book_data[0], book_data[2], book_data[3], (0, 0, 255), 1)
@@ -35,7 +141,6 @@ class BookVizor(QMainWindow):
         except Exception as e:
             print(e)
 
-
     def startThreads(self):
         self.cam_th = CameraThread(self, self.statusbar, self.thresholdSlider)
         self.cam_th.selectImg.connect(self.imgSliceWork)
@@ -43,7 +148,7 @@ class BookVizor(QMainWindow):
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1305, 834)
+        MainWindow.resize(1305, 769)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayout_7 = QtWidgets.QHBoxLayout(self.centralwidget)
@@ -53,8 +158,6 @@ class BookVizor(QMainWindow):
         self.groupBox.setObjectName("groupBox")
         self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.groupBox)
         self.verticalLayout_4.setObjectName("verticalLayout_4")
-        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
         self.verticalLayout_3 = QtWidgets.QVBoxLayout()
         self.verticalLayout_3.setObjectName("verticalLayout_3")
         self.horizontalLayout_5 = QtWidgets.QHBoxLayout()
@@ -76,21 +179,7 @@ class BookVizor(QMainWindow):
         self.thresholdSlider.setObjectName("thresholdSlider")
         self.horizontalLayout_4.addWidget(self.thresholdSlider)
         self.verticalLayout_3.addLayout(self.horizontalLayout_4)
-        self.horizontalLayout_6 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_6.setObjectName("horizontalLayout_6")
-        self.resetBtn = QtWidgets.QPushButton(self.groupBox)
-        self.resetBtn.setObjectName("resetBtn")
-        self.horizontalLayout_6.addWidget(self.resetBtn)
-        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_6.addItem(spacerItem1)
-        self.verticalLayout_3.addLayout(self.horizontalLayout_6)
-        self.horizontalLayout_3.addLayout(self.verticalLayout_3)
-        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_3.addItem(spacerItem2)
-        self.saveImgBtn = QtWidgets.QPushButton(self.groupBox)
-        self.saveImgBtn.setObjectName("saveImgBtn")
-        self.horizontalLayout_3.addWidget(self.saveImgBtn)
-        self.verticalLayout_4.addLayout(self.horizontalLayout_3)
+        self.verticalLayout_4.addLayout(self.verticalLayout_3)
         self.video = QtWidgets.QLabel(self.groupBox)
         self.video.setMinimumSize(QtCore.QSize(640, 480))
         self.video.setText("")
@@ -109,11 +198,8 @@ class BookVizor(QMainWindow):
         self.verticalLayout_2.addWidget(self.tableWidget)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.scanBtn = QtWidgets.QPushButton(self.groupBox_2)
-        self.scanBtn.setObjectName("scanBtn")
-        self.horizontalLayout_2.addWidget(self.scanBtn)
-        spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_2.addItem(spacerItem3)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem1)
         self.deleteBtn = QtWidgets.QPushButton(self.groupBox_2)
         self.deleteBtn.setObjectName("deleteBtn")
         self.horizontalLayout_2.addWidget(self.deleteBtn)
@@ -132,8 +218,8 @@ class BookVizor(QMainWindow):
         self.saveTextBtn = QtWidgets.QPushButton(self.groupBox_3)
         self.saveTextBtn.setObjectName("saveTextBtn")
         self.horizontalLayout.addWidget(self.saveTextBtn)
-        spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem4)
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem2)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.horizontalLayout_7.addWidget(self.groupBox_3)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -147,14 +233,17 @@ class BookVizor(QMainWindow):
         self.thresholdSlider.setMaximum(255)
         self.thresholdSlider.setValue(150)
 
+        self.tableWidget.setColumnCount(1)
+        self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeMode.ResizeToContents)
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.selectBtn.setText(_translate("MainWindow", "Выделить"))
         self.label_2.setText(_translate("MainWindow", "Чувтсвительность: "))
-        self.resetBtn.setText(_translate("MainWindow", "Сбросить"))
-        self.saveImgBtn.setText(_translate("MainWindow", "Сохранить"))
-        self.scanBtn.setText(_translate("MainWindow", "Сканировать"))
         self.deleteBtn.setText(_translate("MainWindow", "Удалить"))
         self.saveTextBtn.setText(_translate("MainWindow", "Сохранить"))
 
@@ -169,13 +258,18 @@ class CameraThread(QThread):
         self.slider = slider
 
         self.isStop = False
+        self.isPaused = False
+
+    def stop(self):
+        self.isStop = True
+        self.wait()
 
     def run(self):
         try:
             cap = cv2.VideoCapture(0)
             self.statusBar.showMessage('Камера подключена')
             while cv2.waitKey(1):
-                if not self.isStop:
+                if not self.isStop and not self.isPaused:
                     ret, frame = cap.read()
                     if not ret:
                         self.statusBar.showMessage('Камера работает неправильно')
@@ -210,6 +304,8 @@ class CameraThread(QThread):
                         self.selectImg.emit((frame, book_img, book_pt1, book_pt2))
                     else:
                         self.selectImg.emit((frame, None))
+                elif self.isPaused:
+                    pass
                 else:
                     cap.release()
                     self.statusBar.showMessage('Камера отключена')
